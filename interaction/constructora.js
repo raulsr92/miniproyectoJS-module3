7ef0fafa2063143d3db6500e136774f4594043e0
Raul Sanchez R. <raul.sr92@gmail.com>
2024-03-06 17:17:36 -0500

    JavaScript - creación de la f constructora del objeto Club de Fútbol

---
+++ interaction/constructora.js
@@ -0,0 +1,37 @@
+
+/* Función constructora para crear los objetos que reresentan a los equipos */
+
+function Teams(name, badge, stadium, city, website, kit1, kit2, kit3, mainPartner, kitSupplier) {
+    this.name = name;
+    this.badge = badge;
+    this.stadium = stadium;
+    this.city = city;
+    this.website = website;
+    this.kit1 = kit1;
+    this.kit2 = kit2;
+    this.kit3 = kit3;
+    this.mainPartner = mainPartner;
+    this.kitSupplier = kitSupplier;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Creación de Objetos */
+
+/*
+
+const manchesterUnited = new Teams("Manchester United", "./imagenes/manchester-United-100x100.png",
+                                    "Old Trafford", "Manchester", "https://www.manutd.com/","./imagenes/camisetas/manchester-united-kit1.png","./imagenes/camisetas/manchester-united-kit2.png","./imagenes/camisetas/manchester-united-kit3.png","./imagenes/partners/manchester-united-partner.png","./imagenes/suppliers/adidas-logo.png")
+
+*/
